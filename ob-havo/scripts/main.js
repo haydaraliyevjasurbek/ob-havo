54ea73330c151f7dcb5070a105d3fc4e8643e132
Jasurbek Haydaraliyev <129217406+haydaraliyevjasurbek@users.noreply.github.com>
2024-06-01 16:13:13 +0500

    Add files via upload

---
+++ ob-havo/scripts/main.js
@@ -0,0 +1,56 @@
+const apiKey = '77d3ed000a882d3dd27d40d512c6c326';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+let test = searchBtn.value 
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    
+    if(response.status == 404){
+        document.querySelector('.eror').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+
+    }else{
+        let data = await response.json();
+
+        document.querySelector('.city').innerHTML = data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+        
+        if(data.weather[0].main == 'Clouds'){
+            weatherIcon.src = 'images/clouds.png'
+        }else if(data.weather[0].main == 'Clear'){
+            weatherIcon.src = 'images/clear.png'
+        }else if(data.weather[0].main == 'Rain'){
+            weatherIcon.src = 'images/rain.png'
+        }else if(data.weather[0].main == 'Drizzle'){
+            weatherIcon.src = 'images/drizzle.png'
+        }else if(data.weather[0].main == 'Mist'){
+            weatherIcon.src = 'images/mist.png'
+        }
+        document.querySelector('.weather').style.display = 'block'
+        document.querySelector('.eror').style.display = 'none'
+    }
+    
+
+    // console.log(data)
+}
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value) 
+   
+    // if(test != ''){
+    //     // console.log('ishlamadi')
+    // }else{
+    //     document.querySelector('.eror').style.display = 'block'
+    //     document.querySelector('.weather').style.display = 'none'
+    // }    
+    
+})
+
+
+// checkWeather()
